025bd921aad0f2dfa85b2a7984869354ef191c0d
Diego <diegogarciaangel25@gmail.com>
2025-10-16 22:31:19 +0200

    implementación del horario

--- swingcr/src/main.js
+++ swingcr/src/main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botones = document.querySelectorAll('#filtro-btn-activo, #filtro-btn');
+    const contenedores = document.querySelectorAll('#tabla-actividades, #tabla-clases');
+    
+    botones.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const targetId = (btn.textContent.includes('Actividades')) ? 'tabla-actividades' : 'tabla-clases';
+            
+            contenedores.forEach(c => c.classList.add('oculto'));
+            botones.forEach(b => {
+                b.id = 'filtro-btn'; 
+            });
+
+            document.getElementById(targetId).classList.remove('oculto');
+            btn.id = 'filtro-btn-activo'; 
+            
+            document.getElementById('titulo-programa').textContent = 
+                (targetId === 'tabla-clases') ? 'Programa de Clases' : 'Programa de Actividades';
+        });
+    });
+});
